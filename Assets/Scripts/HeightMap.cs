d0402d44fc0d2eaa0edafb32cfa5c7d35de6b683
Calvin Kun <calvin188@hotmail.com>
2020-06-19 11:48:13 +0200

    Fixed custom perlin noise map

--- Assets/Scripts/HeightMap.cs
+++ Assets/Scripts/HeightMap.cs
@@ -34,35 +34,38 @@ public class HeightMap : MonoBehaviour
     {
         terrainMesh = new Mesh();
         GetComponent<MeshFilter>().mesh = terrainMesh;
-        GetPerlinValue();
+        GenerateCustomPerlinMap();
         UpdateMesh();
     }
 
-    public void GetPerlinValue() {
+    private void GenerateCustomPerlinMap() {
 
         vertices = new Vector3[(resolution + 1) * (resolution + 1)];
+        NoiseMethod method = Noise.noiseMethods[(int)type][dimensions - 1];
+
+        // Map points have to be between 0-1 range
+        float stepSize = 1f / resolution;
 
         // World coordinates
         Vector3 point00 = transform.TransformPoint(new Vector3(-0.5f,-0.5f));
 		Vector3 point10 = transform.TransformPoint(new Vector3( 0.5f,-0.5f));
 		Vector3 point01 = transform.TransformPoint(new Vector3(-0.5f, 0.5f));
 		Vector3 point11 = transform.TransformPoint(new Vector3( 0.5f, 0.5f));
-
-        NoiseMethod method = Noise.noiseMethods[(int)type][dimensions - 1];
-        // Color has to be between 0-1 range
-        float stepSize = 1f / resolution;
-        // Apply color to every pixel in the texture
-        for (int y = 0, i = 0; y < resolution; y++) {
-            Vector3 point0 = Vector3.Lerp(point00, point01, (y + 0.5f) * stepSize);
-			Vector3 point1 = Vector3.Lerp(point10, point11, (y + 0.5f) * stepSize);
-            for (int x = 0; x < resolution; x++) {
+        
+        // Calculate height for each point on the grid
+        for (int i = 0, z = 0; z <= resolution; z++)
+        {
+            Vector3 point0 = Vector3.Lerp(point00, point01, (z + 0.5f) * stepSize);
+            Vector3 point1 = Vector3.Lerp(point10, point11, (z + 0.5f) * stepSize);
+            for (int x = 0; x <=resolution; x++)
+            {
                 Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) * stepSize);
-                //Vector3 point = new Vector3(x * .3f, y * .3f);
-                float sample = Noise.Sum(method, point, frequency, octaves, lacunarity, persistence);
-				if (type != NoiseMethodType.Value) {
-					sample = sample * 0.5f + 0.5f;
+                // Vector3 point = new Vector3(x * .3f, z * .3f);
+                float y = Noise.Sum(method, point, frequency, octaves, lacunarity, persistence);
+                if (type != NoiseMethodType.Value) {
+					y = y * 4f;
 				}
-                vertices[i] = new Vector3(x, sample * 4, y);
+                vertices[i] = new Vector3(x, y, z);
                 i++;
             }
         }
@@ -96,13 +99,13 @@ public class HeightMap : MonoBehaviour
         }
     }
 
-    void CreateShape()
+    private void GenerateBuildInPerlinMap()
     {
         vertices = new Vector3[(resolution + 1) * (resolution + 1)];
 
-        for (int i = 0, z =0; z<= resolution; z++)
+        for (int i = 0, z = 0; z <= resolution; z++)
         {
-            for (int x = 0; x<=resolution; x++)
+            for (int x = 0; x <=resolution; x++)
             {
                 float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;
                 vertices[i] = new Vector3(x, y, z);
@@ -132,7 +135,6 @@ public class HeightMap : MonoBehaviour
             }
             vert++;
         }
-
     }
 
     void UpdateMesh()
@@ -159,5 +161,15 @@ public class HeightMap : MonoBehaviour
             }
         }
         return mask;
+    }
+
+    private void OnDrawGizmos() {
+        if(vertices != null)
+        {
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                Gizmos.DrawSphere(vertices[i], .1f);
+            }
+        }
     }
 }
