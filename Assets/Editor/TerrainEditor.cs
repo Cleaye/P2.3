356a1b154821cfe20b9e4453c83ac4f60da239c8
Calvin Kun <calvin188@hotmail.com>
2020-06-25 19:00:43 +0200

    Added generate button in inspector + forgot to commit some files

---
+++ Assets/Editor/TerrainEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(HeightMap))]
+public class TerrainEditor : Editor
+{
+    HeightMap terrain;
+    // Start is called before the first frame update
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        GUILayout.BeginHorizontal();
+        if(GUILayout.Button("Generate Terrain"))
+        {
+            terrain.Generate();
+        }
+
+        if(GUILayout.Button("Reset Terrain"))
+        {
+            terrain.ResetTerrainValues();
+        }
+        GUILayout.EndHorizontal();
+    }
+
+    private void OnEnable()
+    {
+        terrain = (HeightMap)target;
+    }
+}
